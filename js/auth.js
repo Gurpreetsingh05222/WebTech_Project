e835306c18b060a3afb0a84481d0b95c98387fac
emarmorrison <emarmorrison@gmail.com>
2021-04-20 21:30:10 -0500

    Validation For Password Length Added

--- js/auth.js
+++ js/auth.js
@@ -1,5 +1,138 @@
 
+$(document).ready(function() {
 
+    // move focus to first text box
+
+   
+    // the handler for the click event of a submit button
+    var ErrArr = [];
+    $("#submit").click(
+
+        function(event) {
+            event.preventDefault();
+            var isValid = false;
+
+            // validate the email entry with a regular expression
+
+            var emailPattern = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\b/;
+
+            var email = $("#email").val().trim();
+
+            if (email == "") {
+               ErrArr.push("The email field is required.");
+                $("#email").next().text("This field is required.");
+
+                isValid = false;
+
+            } else if ( !emailPattern.test(email) ) {
+                 
+                ErrArr.push("Must be a valid email address..");
+               
+                isValid = false;
+
+            } else {
+
+                $("#email").next().text("");
+
+            }
+
+            $("#email").val(email);
+
+            // validate the second email entry
+
+            var email2 = $("#email1").val().trim();
+
+            if (email2 == "") {
+
+                ErrArr.push("Confrm your email field is required.");
+
+                isValid = false;
+
+            } else if (email !== email2 ) {
+
+                ErrArr.push("Email 2 Must equal first email entry.");
+
+                isValid = false;
+
+            } else {
+
+                $("#email1").next().text("");
+
+            }
+
+            $("#email2").val(email2);
+
+            // validate the first name entry
+
+            if ($("#first_name").val() == "") {
+
+                ErrArr.push("Please enter your firstname, this field is required.");               
+
+                isValid = false;
+
+            } else {
+
+                $("#first_name").next().text("");
+
+            }
+
+            // validate the last name entry
+
+            var lastName = $("#last_name").val().trim();
+
+            if (lastName == "") {
+
+                ErrArr.push("Please enter your lastname, this field is required.");                
+
+                isValid = false;
+
+            } else {
+
+                $("#last_name").next().text("");
+
+            }
+
+            $("#last_name").val(lastName);
+
+                   
+            var password = $('#password1').val();
+           
+            var confirm_password = $('#confirm_password').val();
+            
+            if(password == "")
+            {
+                ErrArr.push("Your Password cannot be empty")
+            }
+            
+            if(password != confirm_password)
+            {
+                ErrArr.push("Your passwords do not match </b>")
+            }
+            
+            if (isValid == false) {
+
+            }
+
+            if(ErrArr.length > 0) {
+
+                alert(ErrArr.join("\n"));
+
+                ErrArr = [];
+
+                return false;
+              }
+              else{
+               
+                $(location).attr('href','login.html')
+
+              }
+
+        } // end function
+
+    );  // end submit
+
+}); // end ready
+/*
 function registration_validation() {
     
     var fname =  document.forms["RegForm"]["firt_name"];
@@ -27,13 +160,13 @@ function registration_validation() {
         return false;
     }
 
-    /*if (phone.value == "") {
+if (phone.value == "") {
         window.alert(
           "Please enter your telephone number.");
         phone.focus();
         return false;
     }
-    */
+    
     if (password1.value == "") {
         window.alert("Please enter your password");
         password.focus();
@@ -47,4 +180,4 @@ function registration_validation() {
     } 
 
     return true;
-}
+}*/
