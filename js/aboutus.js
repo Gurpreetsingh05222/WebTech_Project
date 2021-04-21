2a2e83e2adf2af430f05dccd07080ab2ff8e3a3b
kriti269 <kriti269@gmail.com>
2021-04-21 12:38:56 -0400

    - updated sitemap - updated animation on about us

--- js/aboutus.js
+++ js/aboutus.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-  $(".aboutus-content").slideDown(2000);
+  $(".aboutus-content").slideDown(500);
   $('.aboutus-content img').css('display', 'block');
   $('.aboutus-content img').animate({
     display: "block",
     width: "50%",
     opacity: 1
-  }, 1000);
+  }, 500);
 
   $("#date").text((new Date).getFullYear());
 });
